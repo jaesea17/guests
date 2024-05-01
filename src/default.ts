91dd92f2d3e939025cdd9e8746b3e17f2214ba6f
Oforkaja Joseph <oforkajajoseph@gmail.com>
2024-05-01 17:23:01 +0100

    refactored-i

--- src/default.ts
+++ src/default.ts
@@ -1,5 +1,5 @@
-export const baseUrl = "http://localhost:19190";
-// export const baseUrl = "https://guests-api.onrender.com";
+// export const baseUrl = "http://localhost:19190";
+export const baseUrl = "https://guests-api.onrender.com";
 export enum FetchPoint {
   GUESTS = "guests",
 }
